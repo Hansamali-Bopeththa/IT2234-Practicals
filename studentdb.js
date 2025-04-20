dcdb9b65331b243eadd8927262bc4d4932e38e34
Hansamali-Bopeththa <hansamalib002@gmail.com>
2025-04-20 22:24:01 +0530

    student Database

---
+++ studentdb.js
@@ -0,0 +1,9 @@
+let students =[
+    {Reg_No: '2233', Name: 'Ama', age: 22, Course: 'IT', gender: 'Female'},
+    {Reg_No: '2222', Name: 'Sahan', age: 23, Course: 'Science' ,gender: 'male'},
+    {Reg_No: '2244', Name: 'Nethmi', age: 21, Course: 'Maths', gender: 'Female'},
+    {Reg_No: '2255', Name: 'Yash', age: 24, Course: 'IT', gender: 'Female'},
+    {Reg_No: '2266', Name: 'Anudi', age: 22, Course: 'IT', gender: 'Female'},
+];
+
+module.exports = students;
