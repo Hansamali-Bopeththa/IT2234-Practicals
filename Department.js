c7d834d25187ce4bf77296bfece7ad2da1d796c3
Hansamali-Bopeththa <hansamalib002@gmail.com>
2025-06-01 16:25:52 +0530

    Add files via upload

---
+++ Department.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const departmentSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    employees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Employee' }]
+});
+
+const Department = mongoose.model('Department', departmentSchema);
+
+module.exports = Department;
+
+/*
+const departments = [
+    { _id: "D1", name: "HR", employeeID: [1, 3] },
+    { _id: "D2", name: "Finance", employeeID: [2, 5] },
+    { _id: "D3", name: "IT", employeeID: [4] }
+    
+]
+Department.insertMany(departments);
+
+*/
+
+module.exports=Department
